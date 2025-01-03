d74b2547ce18e7271721431596571dc99e3979c3
connell.alan.g <alan.g.connell.ctr@army.mil>
2025-01-03 16:57:08 -0500

    Added hrm
    now hrm and power both being measured

--- src/workout.py
+++ src/workout.py
@@ -1,6 +1,7 @@
 import asyncio
 from bleak import BleakClient
 from src.cycling_power_service import CyclingPowerService
+from src.heart_rate_service import HeartRateService
 
 class Workout():
     def __init__(self):
@@ -8,7 +9,7 @@ class Workout():
         self.kickr_address = "FB:4A:F1:1F:1B:73"
 
     def power_measurement_handler(self, measurement):
-        print(f'power: {measurement}')
+        print(f'power: {measurement.instantaneous_power}')
 
     async def connect_power_meter(self):
         print('connecting to power meter')
@@ -24,13 +25,39 @@ class Workout():
         while(True):
             await asyncio.sleep(1)
 
+
+
+    def hrm_measurement_handler(self, measurement):
+        print(f'hrm: {measurement.bpm}')
+
+    async def connect_hrm(self):
+        print('connecting to heart rate monitor')
+        hrm = BleakClient(self.hrm_address)
+        await hrm.connect()
+        return hrm
+
+    async def start_hrm(self, hrm_client):
+        print('Recording hrm data')
+        hrms = HeartRateService(hrm_client)
+        hrms.set_hr_measurement_handler(self.hrm_measurement_handler)
+        await hrms.enable_hr_measurement_notifications()
+        while(True):
+            await asyncio.sleep(1)
+
+
+
     def start_workout(self):
         print('starting workout')
         
         loop = asyncio.get_event_loop()
+
         power_client = loop.run_until_complete(self.connect_power_meter())
+        hrm_client = loop.run_until_complete(self.connect_hrm())
 
         if power_client and power_client.is_connected:
             loop.create_task(self.start_power_meter(power_client))
+
+        if hrm_client and hrm_client.is_connected:
+            loop.create_task(self.start_hrm(hrm_client))
         
         loop.run_forever()
